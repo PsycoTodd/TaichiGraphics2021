28514c84c2f284874d022b5935f96541699f542c
PsychoTodd <toddzhang1@gmail.com>
2021-10-24 16:18:03 -0700

    assignment 1 with readme update, to get URL for the gif.

--- Assignment1.py
+++ Assignment1.py
@@ -3,7 +3,8 @@ import taichi as ti
 ti.init(arch=ti.gpu)
 
 n = 320
-pixels = ti.field(dtype = float, shape = (n * 2, n))
+PI = 3.1415926
+pixels = ti.field(dtype = float, shape = (n, n, 3))
 
 @ti.func
 def complex_sqr(z):
@@ -11,19 +12,28 @@ def complex_sqr(z):
 
 
 @ti.kernel
-def paint(t: float):
-    for i, j in pixels:
-        c = ti.Vector([-0.8, ti.cos(t) * 0.2])
-        z = ti.Vector([i / n - 1, j / n - 0.5]) * 2
+def render(t: float):
+    for i, j, k in pixels:
+        c = ti.Vector([0.285, ti.sin(t  + k * PI / 3.0 ) * 0.01])
+        z = ti.Vector([i / n - 0.5, j / n - 0.5]) * 2
         iter = 0
         while z.norm() < 20 and iter < 50:
             z = complex_sqr(z) + c
             iter += 1
-        pixels[i, j] = 1 - iter * 0.02
+        pixels[i, j, k] = iter * 0.02
 
-gui = ti.GUI("Julia Set Todd", res=(n * 2, n))
+def main(output_img=False):
+    gui = ti.GUI("Julia Set Todd", res=(n, n))
+    for ts in range(1000000):
+        if gui.get_event(ti.GUI.ESCAPE):
+            exit()
+        
+        render(ts * 0.03)
+        gui.set_image(pixels)
+        if output_img:
+            gui.show(f'{ts:04d}.jpg')
+        else:
+            gui.show()
 
-for i in range(1000000):
-    paint(i * 0.03)
-    gui.set_image(pixels)
-    gui.show()
+if __name__ == '__main__':
+    main(output_img=False)
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # TaichiGraphics2021
-This is the side work to have fun with Taichi Graphics course 2021 https://space.bilibili.com/1779922645?spm_id_from=333.788.b_765f7570696e666f.1
+This is the side work to have fun with [Taichi Graphics course 2021](https://space.bilibili.com/1779922645?spm_id_from=333.788.b_765f7570696e666f.1)
+
+### Assignment 1 Julia set RGB
+**Control the 3 channel with difference in the sin function.**
+![alt text][JuliaSet]
+
+
+
+
+[JuliaSet]: 
